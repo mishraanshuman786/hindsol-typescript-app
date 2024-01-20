37cf14d84823a4342a17b43e0b977c48edb813fc
Akash <ak931457@gmail.com>
2024-01-20 11:18:10 +0530

    Serices div decoration

--- src/components/ServicesComponent.jsx
+++ src/components/ServicesComponent.jsx
@@ -6,122 +6,123 @@ let arr = [
   {
     heading: 'Metro Cabs',
     para: 'I need taxi App.',
-    color: 'green-500',
+    color: 'bg-green-500',
   },
   {
     heading: 'E-Commerce Application',
     para: 'I need E-Commerce App.',
-    color: 'green-500',
+    color: 'bg-red-500',
   },
   {
     heading: 'Food Application',
     para: 'I need Food App.',
-    color: 'green-500',
+    color: 'bg-blue-700',
   },
-  {
-    heading: 'Medical Application',
-    para: 'I need Health App.',
-    color: 'green-500',
-  },
+ 
   {
     heading: 'E-Learning Application',
     para: 'I need E-Learning App.',
-    color: 'green-500',
+    color: 'bg-red-600',
   },
   {
     heading: 'Chat Application',
     para: 'I need Chat message App.',
-    color: 'green-500',
+    color: 'bg-orange-400',
   },
   {
     heading: 'Real-Estate Application',
     para: 'I need Real State App.',
-    color: 'green-500',
+    color: 'bg-green-400',
   },
   {
     heading: 'Banking Application',
     para: 'I need finance/banking App',
-    color: 'green-500',
+    color: 'bg-yellow-600',
   },
   {
     heading: 'Tours and Travels Application.',
     para: 'I need Tours and Travels App.',
-    color: 'green-500',
+    color: 'bg-orange-600',
   },
   {
     heading: 'Dating Application',
     para: 'I need dating App.',
-    color: 'green-500',
+    color: 'bg-yellow-600',
   },
   {
     heading: 'Crypto-Currency Application',
     para: 'I need Crypto-Currency App.',
-    color: 'green-500',
+    color: 'bg-violet-500',
   },
   {
     heading: 'Business Application',
     para: 'I need Business Website/App.',
-    color: 'green-500',
+    color: 'bg-red-600',
   },
   {
     heading: 'Bot',
     para: 'I need Bot.',
-    color: 'green-500',
+    color: 'bg-yellow-600',
   },
   {
     heading: 'Automobile Application',
     para: 'I need Automobile App.',
-    color: 'green-500',
+    color: 'bg-green-400',
   },
   {
     heading: 'Pharma Application',
     para: 'I need Pharma App.',
-    color: 'green-500',
+    color: 'bg-red-500',
   },
   {
     heading: 'Video Streaming Application ',
     para: 'I need Video Streaming App.',
-    color: 'green-500',
+    color: 'bg-blue-700',
   },
   {
-    heading: 'Lohgistics Application',
+    heading: 'Logistics Application',
     para: 'I need Logistics App.',
-    color: 'green-500',
+    color: 'bg-orange-600',
   },
   {
     heading: 'Job Portal Application',
     para: 'I need Job Portal App.',
-    color: 'green-500',
+    color: 'bg-orange-600',
   },
   {
     heading: 'Event Application',
     para: 'I need Event App.',
-    color: 'green-500',
+    color: 'bg-red-600',
   },
   {
     heading: 'Business Application',
     para: 'I need Business Website/App.',
-    color: 'green-500',
+    color: 'bg-red-600',
   },
   {
     heading: 'Shadi Application',
     para: 'I need Shadi Application.',
-    color: 'green-500',
+    color: 'bg-red-500',
   },
   {
     heading: 'Education Application',
     para: 'I need Education App.',
-    color: 'green-500',
+    color: 'bg-green-400',
+  },
+  {
+    heading:"Medical Application",
+    para:"I need Medical App.",
+    color:"bg-red-500"
   },
   {
     heading: 'ERP Solution',
     para: 'I need ERP Solution.',
-    color: 'green-500',
+    color: 'bg-green-400',
   },
   {
     heading: 'Hindsol Application',
     para: 'I need HRM App.',
-    color: 'green-500',
+    color: 'bg-orange-600',
   }
 ];
 
@@ -131,13 +132,14 @@ function ServicesComponent() {
     <div className='gap-4 px-8 mb-4 grid grid-cols-4 grid-rows-6'>
       {arr.map((element, index) => {
         let image = `/images/${index}.png`;
+       
         return (
           <div
             key={index}
-            className={` ${styles.containerHover} h-[200px] relative overflow-hidden  border border-gray-700 bg-green-600 `}
+            className={` ${styles.containerHover} h-[200px] relative bg-blue overflow-hidden ${element.color} `}
           >
-            <div className='z-10 w-[60%] text-white  absolute'>
-              <h1 className='text-[30px] font-bold'>{element.heading}</h1>
+            <div className='z-10 w-[60%] ml-2 text-white  absolute'>
+              <h1 className='text-[25px] font-bold'>{element.heading}</h1>
               <h4>{element.para}</h4>
             </div>
 
@@ -146,7 +148,7 @@ function ServicesComponent() {
               alt={element.heading}
               width={120}
               height={120}
-              className={`${styles.imageHover} mt-[70px] ml-[170px] `}
+              className={`${styles.imageHover} mt-[65px] ml-[152px] `}
             />
           </div>
         );
