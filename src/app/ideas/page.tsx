52d0115713d05d67f818a38cd7a0c67a53b6f441
Akash <ak931457@gmail.com>
2024-02-02 17:38:19 +0530

    successfully intiating slider on the career page

--- src/app/ideas/page.tsx
+++ src/app/ideas/page.tsx
@@ -1,37 +1,24 @@
-"use client"
-import Navbar from '@/components/Navbar';
-import React,{useEffect,useState} from 'react';
-import Pulsating from '@/components/Pulsating';
+"use client";
+import Navbar from "@/components/Navbar";
+import React, { useEffect, useState } from "react";
+import Pulsating from "@/components/Pulsating";
 
 function Ideas() {
-
   const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate a delay to demonstrate the loading animation
-    const timeout = setTimeout(() => {
-      setLoading(false);
-    }, 2000); // Adjust the duration as needed
-
-    return () => clearTimeout(timeout);
-  }, []);
 
   return (
     <div>
       {/* Navbar */}
-      <Navbar/>
+      <Navbar />
 
-        {/* Pulsating div for loading animation */}
-        {
-        (loading && (<Pulsating/>))
-       }
-        {/* pulsating end */}
+      {/* Pulsating div for loading animation */}
+      {loading && <Pulsating />}
 
-       <div className={`container mx-auto ${loading ? "hidden" : ""}`}>
-            Ideas
-       </div> 
+      <div className={`container mx-auto ${loading ? "hidden" : ""}`}>
+        Ideas
+      </div>
     </div>
-  )
+  );
 }
 
 export default Ideas;
