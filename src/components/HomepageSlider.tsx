e2a45717ad559cf078a89271297f4925e54bbc37
anshuman619 <mishraanshuman619@gmail.com>
2024-02-12 15:02:58 +0530

    adding sidebar

--- src/components/HomepageSlider.tsx
+++ src/components/HomepageSlider.tsx
@@ -15,7 +15,7 @@ function HomepageSlider() {
         data-carousel="slide"
       >
         {/* <!-- Carousel wrapper --> */}
-        <div className="relative h-56 overflow-hidden border border-black rounded-lg md:h-[550px] ">
+        <div className="relative h-56 overflow-hidden border border-black rounded-lg md:h-[400px] ">
           {/* <!-- Item 1 --> */}
           <div className="hidden duration-700 h-full  ease-in-out" data-carousel-item>
             <img
