86a35454e8d32a49f487f368890e5e5349d3e91c
anshuman619 <mishraanshuman619@gmail.com>
2024-02-12 22:43:37 +0530

    adding sidebar

--- src/components/CareerPageSlider.tsx
+++ src/components/CareerPageSlider.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 
-
 function HomepageSlider() {
   return (
     <div className="pt-[40px]">
-
-     <div className="container text-[25px]  md:text-[50px] font-bold">
-        <h2 ><span className="text-green-400">Life</span> At HINDSOL</h2>
-        
+      <div className="container text-[25px]  md:text-[50px] font-bold">
+        <h2>
+          <span className="text-green-400">Life</span> At HINDSOL
+        </h2>
       </div>
 
       {/* Carousel component */}
@@ -20,7 +19,10 @@ function HomepageSlider() {
         {/* <!-- Carousel wrapper --> */}
         <div className="relative h-44   overflow-hidden  rounded-lg md:h-[540px] ">
           {/* <!-- Item 1 --> */}
-          <div className="hidden duration-700 h-full  ease-in-out" data-carousel-item>
+          <div
+            className="hidden duration-700 h-full  ease-in-out"
+            data-carousel-item
+          >
             <img
               src="/career/careerSlider1.svg"
               className="absolute  block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2  object-cover"
@@ -28,32 +30,16 @@ function HomepageSlider() {
             />
           </div>
           {/* <!-- Item 2 --> */}
-          <div className="hidden duration-700  h-full ease-in-out" data-carousel-item>
-          <img
-              src="/slider-images/homepageSlider2.svg"
+          <div
+            className="hidden duration-700  h-full ease-in-out"
+            data-carousel-item
+          >
+            <img
+              src="/career/careerSlider2.svg"
               className="absolute  block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2  object-cover"
               alt="..."
             />
           </div>
-         
-        </div>
-        {/* <!-- Slider indicators --> */}
-        <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="true"
-            aria-label="Slide 1"
-            data-carousel-slide-to="0"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="false"
-            aria-label="Slide 2"
-            data-carousel-slide-to="1"
-          ></button>
-         
         </div>
         {/* <!-- Slider controls --> */}
         <button
@@ -63,7 +49,7 @@ function HomepageSlider() {
         >
           <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
             <svg
-              className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+              className="w-4 h-4 text-white rtl:rotate-180"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
