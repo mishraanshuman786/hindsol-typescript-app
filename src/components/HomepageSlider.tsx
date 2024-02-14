b1946279b55dd45cc06848884cde2047d3b8ce48
anshuman619 <mishraanshuman619@gmail.com>
2024-02-14 13:33:08 +0530

    changing navbar

--- src/components/HomepageSlider.tsx
+++ src/components/HomepageSlider.tsx
@@ -45,7 +45,7 @@ function HomepageSlider() {
              </div>
              {/* mobile */}
              <div className="flex md:hidden">
-             <h1 className="text-[15px] py-14 text-center text-white ">
+             <h1 className="text-[15px] py-11 text-center text-white ">
              Want to make your online presence shine brighter than a shooting star? Let Hindsol illuminate your website with our expert development services!
                   </h1>
                   <img src="/slider-images/image2.png" className="w-[200px] h-[200px]" />
