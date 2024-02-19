8513fd760feb07dd0219d52f306a67b752e7075c
anshuman619 <mishraanshuman619@gmail.com>
2024-02-19 12:48:13 +0530

    monday

--- src/app/success/page.jsx
+++ src/app/success/page.jsx
@@ -1,11 +1,10 @@
 "use client"
 import AlertPopup from "../../components/AlertPopup";
-import {useCookie} from "../../library/util/useCookie";
+
 export default function Success(){
-    const statusCookieValue = useCookie("status");
-    console.log(statusCookieValue);
+   
     return (<div>
         <h1>Payment successfull</h1>
-        {statusCookieValue && <AlertPopup message="Payment success" />}
+        <AlertPopup message="Payment success" />
         </div>)
 }
