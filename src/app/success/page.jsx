206b0eb6c43849c075088b2e62206fdbc5875b02
mishraanshuman786 <125239639+mishraanshuman786@users.noreply.github.com>
2024-02-19 16:22:24 +0530

    change in the payment route

--- src/app/success/page.jsx
+++ src/app/success/page.jsx
@@ -2,6 +2,7 @@
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import "../globals.css";
 
 
 export default function Success() {
@@ -14,7 +15,7 @@ export default function Success() {
       if (counter > 0) {
         setCounter(counter - 1);
       } else {
-        router.push('/skillDev');
+        router.push('/');
       }
     }, 1000);
 
@@ -24,17 +25,18 @@ export default function Success() {
   }, [router, counter]);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-blue-800">
+    <div className="relative flex flex-col items-center justify-center h-screen">
+      <div className="absolute inset-0 bg-gradient-to-r from-blue-500 to-purple-500 opacity-25 z-0 animate-flicker" />
       <Image
-        src="/assets/payment-success.gif"
+        src="/assets/payment.gif"
         alt="Payment Success"
-        width="128"
-        height="128"
+        width="400"
+        height="400"
       />
-      <h1 className="mt-6 text-2xl font-semibold text-white">
+      <h1 className="mt-6 text-2xl font-semibold text-gray-900 z-10">
         Payment Successfull.
       </h1>
-      <p className="mt-3 text-lg text-white">
+      <p className="mt-3 text-lg text-gray-600 z-10">
         You will be redirected to the SkillDev page in {counter} seconds.
       </p>
     </div>
