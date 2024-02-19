3a5f55ab433cf1e5b9291144c729b9781c6c25de
mishraanshuman786 <125239639+mishraanshuman786@users.noreply.github.com>
2024-02-19 14:36:47 +0530

    change in the payment route

--- src/app/success/page.jsx
+++ src/app/success/page.jsx
@@ -1,13 +1,35 @@
-import { getCookie } from 'cookies';
-import AlertPopup from '../../components/AlertPopup';
+"use client"
+import Image from 'next/image';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Success() {
-  const cookie = getCookie('status', { parse: true });
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace('/skillDev');
+    }, 10000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [router]);
 
   return (
-    <div>
-      <h1>Payment successfull</h1>
-      {cookie && <AlertPopup message="Payment success" />}
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-50">
+      <Image
+        src="/images/payment-success.svg"
+        alt="Payment Success"
+        width="128"
+        height="128"
+      />
+      <h1 className="mt-6 text-2xl font-semibold text-gray-900">
+        Payment Successfull.
+      </h1>
+      <p className="mt-3 text-lg text-gray-600">
+        You will be redirected to the SkillDev page in 10 seconds.
+      </p>
     </div>
   );
 }
