5a21af5d1413ab4b13002b71e48f8b255e689368
anshuman619 <mishraanshuman619@gmail.com>
2024-02-20 21:26:58 +0530

    saving userid

--- src/app/api/it/payment/initiatePayment/[id]/route.js
+++ src/app/api/it/payment/initiatePayment/[id]/route.js
@@ -12,9 +12,9 @@ async function callPhonePey(muid,amount) {
     merchantTransactionId: transactionId,
     merchantUserId: muid,
     amount: amount*100,
-    redirectUrl: `https://hindsol.com/api/it/payment/status/${transactionId}`,
+    redirectUrl: `https://hindsol.com/api/it/payment/status/${muid}`,
     redirectMode: "POST",
-    callbackUrl: `https://hindsol.com/api/it/payment/status/${transactionId}`,
+    callbackUrl: `https://hindsol.com/api/it/payment/status/${muid}`,
     paymentInstrument: {
       type: "PAY_PAGE",
     },
